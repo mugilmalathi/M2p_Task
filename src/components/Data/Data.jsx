8d80ba33799e230c8627cd56a5f00ecf2a6657cc
Mugilarasan <mugilarasan@M2P-LAP-1078.local>
2023-03-21 10:58:15 +0530

    update

--- src/components/Data/Data.jsx
+++ src/components/Data/Data.jsx
@@ -15,103 +15,39 @@ import nodata from "../../assets/Images/nodata.png";
 import { useToast } from "@chakra-ui/react";
 
 const Data = ({ filtername }) => {
-
   const todo = useSelector((store) => store.TodoReducer);
   const done = useSelector((store) => store.doneTodoreducer);
   const dispatch = useDispatch();
   const toast = useToast();
-  const[state, setState]=useState(false)
+  const [state, setState] = useState(false);
 
-  useEffect(()=>{
-    dispatch(getTodo())
-    dispatch(getDoneTodo())
-  },[state])
+  useEffect(() => {
+    dispatch(getTodo());
+    dispatch(getDoneTodo());
+  }, [state]);
 
   return (
     <>
-    {(todo && todo.length > 0 && filtername === "Filter") ||
-    filtername === "All" ? (
-      <div className="data">
-        {todo.map((el, i) => {
-          return (
-            <div className="datalist">
-              <div>{i+1}</div>
-              <div>{el.todo}</div>
-              <div>
-                <button
-                  className="datadone"
-                  onClick={() => {
-                    dispatch(doneTodo(el));
-                    dispatch(updateTodo(el))
-                    toast({
-                      title: `${el.todo} marked as done.!`,
-                      status: "success",
-                      duration: 5000,
-                      isClosable: true,
-                    });
-                  }}
-                >
-                  <BsCheckSquareFill />
-                </button>
-              </div>
-              <div>
-                <button
-                  className="datadelete"
-                  onClick={() => {
-                    dispatch(deleteTodo(el._id));
-                    setState((prev)=>!prev)
-                    dispatch(getTodo())
-                    toast({
-                      title: `${el.todo} task deleted.!`,
-                      status: "error",
-                      duration: 5000,
-                      isClosable: true,
-                    });
-                  }}
-                >
-                  <BsFillTrashFill />
-                </button>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    ) : filtername === "Done" ? (
-      <div className="data">
-        {done.map((el, i) => {
-          return (
-            <div className="datalist">
-              <div>{i + 1}</div>
-              <div>{el.todo}</div>
-              <div>
-                <button
-                  className="datadelete"
-                  onClick={() => {
-                    dispatch(deleteDoneTodo(el._id))
-                    setState((prev)=>!prev)
-                      dispatch(getDoneTodo())
-                  }}
-                >
-                  <BsFillTrashFill />
-                </button>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    ) : filtername === "Pending" ? (
-      <div className="data">
-        {todo.map((el, i) => {
-          if (el.pending === false) {
+      {(todo && todo.length > 0 && filtername === "Filter") ||
+      filtername === "All" ? (
+        <div className="data">
+          {todo.map((el, i) => {
             return (
               <div className="datalist">
-                <div>{i+1}</div>
+                <div>{i + 1}</div>
                 <div>{el.todo}</div>
                 <div>
                   <button
                     className="datadone"
                     onClick={() => {
                       dispatch(doneTodo(el));
+                      dispatch(updateTodo(el));
+                      toast({
+                        title: `${el.todo} marked as done.!`,
+                        status: "success",
+                        duration: 5000,
+                        isClosable: true,
+                      });
                     }}
                   >
                     <BsCheckSquareFill />
@@ -121,9 +57,15 @@ const Data = ({ filtername }) => {
                   <button
                     className="datadelete"
                     onClick={() => {
-                      dispatch(deleteTodo(el._id))
-                      setState((prev)=>!prev)
-                      dispatch(getTodo())
+                      dispatch(deleteTodo(el._id));
+                      setState((prev) => !prev);
+                      dispatch(getTodo());
+                      toast({
+                        title: `${el.todo} task deleted.!`,
+                        status: "error",
+                        duration: 5000,
+                        isClosable: true,
+                      });
                     }}
                   >
                     <BsFillTrashFill />
@@ -131,15 +73,70 @@ const Data = ({ filtername }) => {
                 </div>
               </div>
             );
-          }
-        })}
-      </div>
-    ) : (
-      <div className="nodata">
-        <img src={nodata} />
-      </div>
-    )}
-
+          })}
+        </div>
+      ) : filtername === "Done" ? (
+        <div className="data">
+          {done.map((el, i) => {
+            return (
+              <div className="datalist">
+                <div>{i + 1}</div>
+                <div>{el.todo}</div>
+                <div>
+                  <button
+                    className="datadelete"
+                    onClick={() => {
+                      dispatch(deleteDoneTodo(el._id));
+                      setState((prev) => !prev);
+                      dispatch(getDoneTodo());
+                    }}
+                  >
+                    <BsFillTrashFill />
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : filtername === "Pending" ? (
+        <div className="data">
+          {todo.map((el, i) => {
+            if (el.pending === false) {
+              return (
+                <div className="datalist">
+                  <div>{el.todo}</div>
+                  <div>
+                    <button
+                      className="datadone"
+                      onClick={() => {
+                        dispatch(doneTodo(el));
+                      }}
+                    >
+                      <BsCheckSquareFill />
+                    </button>
+                  </div>
+                  <div>
+                    <button
+                      className="datadelete"
+                      onClick={() => {
+                        dispatch(deleteTodo(el._id));
+                        setState((prev) => !prev);
+                        dispatch(getTodo());
+                      }}
+                    >
+                      <BsFillTrashFill />
+                    </button>
+                  </div>
+                </div>
+              );
+            }
+          })}
+        </div>
+      ) : (
+        <div className="nodata">
+          <img src={nodata} />
+        </div>
+      )}
     </>
   );
 };
